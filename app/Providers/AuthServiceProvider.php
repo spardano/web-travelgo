243aed401ec6d3da949a403947cfea632a39da77
sakti pardano <Sakti Pardano>
2023-03-28 22:42:36 +0700

    commit pertama project travelgo web dan webservice 3/28/2023

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verifikasi Email')
+                ->line('Tekan tombol dibawah ini untuk verifikasi email.')
+                ->action('Verifikasi Email', $url);
+        });
+    }
+}
---
+++ config/overlook.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'includes' => [
+        // App\Filament\Resources\UserResource::class,
+        // App\Filament\Resources\Blog\AuthorResource::class,
+    ],
+    'excludes' => [
+        // App\Filament\Resources\Blog\AuthorResource::class,
+    ],
+];
